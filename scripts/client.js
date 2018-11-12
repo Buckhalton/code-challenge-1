3e8d7cfc9aee5ac0437d515285b4cb09715b69e3
Isaiah Buckhalton <Isaiah.Buckhalton@gmail.com>
2018-11-12 13:46:28 -0600

    adds swap and delete buttons.

--- scripts/client.js
+++ scripts/client.js
@@ -1,20 +1,35 @@
 $(document).ready(readyNow);
 
-function createDiv() {
-    $('#container').append(`<div></div>`);
+
+let buttonCounter = 0;
+function createDiv(){
+    let divDisplay = `
+    <div id="generatedDiv" class="red">
+        <p>${buttonCounter++}</p>
+        <button class="swap">Swap</button>
+        <button class="delete">Delete</button>
+    </div>`
+    
+    $('#container').append(divDisplay);
 }
 
+function swapColors(){
+    if($(this).parent().attr('class') == 'red'){
+        $(this).parent().removeClass('red');
+        $(this).parent().addClass('yellow');
+    } else if ($(this).parent().attr('class') === 'yellow') {
+        $(this).parent().removeClass('yellow');
+        $(this).parent().addClass('red');
+    }
+}
 
-
-
-
-
-
-
-
-
+function deleteDiv(){
+    $(this).parent().remove();
+}
 
 function readyNow() {
     $('#generateDiv').on('click', createDiv);
+    $('#container').on('click', '.swap', swapColors);
+    $('#container').on('click', '.delete', deleteDiv);
 }
 
